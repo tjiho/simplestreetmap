ac5baf6dbeba172fb23a0d81ba092a37f1abc77a
Tom Darboux <tjiho@localhost.localdomain>
2019-09-23 00:29:53 +0200

    refactor js code put search on its own class

---
+++ static/js/search.js
@@ -0,0 +1,54 @@
+class Search {
+  constructor(map) {
+    this.lazySearch = _.debounce(this.search, 300)
+
+    document.getElementById("search-input").addEventListener("input", (e) => {
+      this.lazySearch(e.target.value)
+    });
+
+    this.currentSearchMarker = null
+    this.map = map
+  }
+
+  clickOnResult(result) {
+    if (this.currentSearchMarker)
+      this.currentSearchMarker.parentNode.removeChild(currentSearchMarker);
+
+    this.currentSearchMarker = document.createElement('div');
+    this.currentSearchMarker.className = 'marker';
+
+    new mapboxgl.Marker(this.currentSearchMarker)
+        .setLngLat([result.lon,result.lat])
+        .addTo(map);
+
+    this.map.flyTo({center: [result.lon,result.lat], zoom: 13});
+  }
+
+  search(query) {
+    axios.get('https://nominatim.openstreetmap.org/search?format=json&q='+query)
+    .then((response) => {
+      this.cleanSearchResults()
+      response.data.slice(0, 5).forEach( (res) => this.displaySearchResult(res))
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  }
+
+  displaySearchResult(result) {
+    let domResult = document.createElement("div")
+    domResult.classList.add("search-results__result")
+    domResult.innerText = result.display_name
+    domResult.addEventListener("click", (e) => {
+      this.clickOnResult(result)
+    })
+    document.getElementById("search-results").appendChild(domResult)
+  }
+
+  cleanSearchResults() {
+    document.getElementById("search-results").innerHTML = ""
+  }
+
+}
