d3aaacc544298b54555dd374829b5a495b9545a1
Tom Darboux <tjiho@localhost.localdomain>
2021-12-20 05:10:49 +0100

    Save position in url

---
+++ static/js/tools/parseHashCoordinates.js
@@ -0,0 +1,9 @@
+export function parseHashCoordinates(hash, defaultLng, defaultLat, defaultZoom) {
+  const coordinates = hash.replace('#map=','').split('/')
+
+  return {
+    lng: coordinates?.[2] || defaultLng,
+    lat:coordinates?.[1] || defaultLat,
+    zoom:coordinates?.[0] || defaultZoom
+  }
+}
