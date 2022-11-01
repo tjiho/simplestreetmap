9e81fdeb2cc5cc0d6097b522dbb647c9f97cfbe5
Gaël Berthaud-Müller <blacksponge@tuta.io>
2022-11-01 18:36:08 +0100

    fix search component

--- src/components/searchComponent.js
+++ src/components/searchComponent.js
@@ -1,31 +1,35 @@
 import { html, Component, render, createContext, useState, useEffect } from '../../../static/vendor/preact/standalone.module.js';
 import { fetchSearchResult } from '../tools/api.js'
 
-export default function search() {
-  const [results, setResults] = useState(0);
+const CITY_TYPES = ['city', 'town', 'village'];
 
+export default function search(onResultSelected = () => {}) {
+  const [results, setResults] = useState([]);
 
-  
-
-
-  async function onInputSearch(e) {
-		console.log(e)
+  function onInputSearch(e) {
 		const searchValue = e.target.value
-		useEffect(() => {
-    	const results = await fetchSearchResult(searchValue)
-    	setResults(results)
-  	}, []);
-  }
 
+    fetchSearchResult(searchValue).then(setResults)
+  }
 
   return html`
       <div>
-        <input type="search" onInput=${}/>
-        <div class="results">	
-        </div>
+        <input type="search" onInput=${onInputSearch}/>
+        <ul class="results">
+          ${results.map(searchResult => result({...searchResult, onResultSelected}))}
+        </ul>
       </div>
     `
 }
 
+function result({ type, name, coord, context, onResultSelected }) {
+  const isCity = CITY_TYPES.includes(type);
 
-
+  return html`
+    <li>
+      ${name}
+      <span class="context" onClick=${(e) => onResultSelected(coord)}>${context.join(', ')}</span>
+      ${isCity ? html`<img src="/static/images/helium/home.svg" />` : ''}
+    </li>
+  `
+}
