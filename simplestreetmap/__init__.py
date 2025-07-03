5312c6a6e329c969674d6413ed28e5d428c21d98
tjiho <tjiho@ppsfleet.navy>
2025-07-04 00:57:48 +0200

    Set front as standalone static html

--- simplestreetmap/__init__.py
+++ simplestreetmap/__init__.py
@@ -12,6 +12,7 @@ from .views import (
 
 __version__ = '0.1'
 
+
 def make_app():
     ssm_config = get_config()
     search_adapter = AddokSearchAdapter(ssm_config['search']['addok'])
@@ -19,23 +20,25 @@ def make_app():
     brouter_adapter = BrouterJourneyAdapter()
 
     with Configurator() as config:
-        config.include('pyramid_jinja2')
-        config.add_jinja2_search_path('templates')
-        config.add_static_view('static', 'static')
-
         config.add_route('search', '/api/v1/search')
         config.add_route('reverse', '/api/v1/reverse')
         config.add_route('itinerary', '/api/v1/itinerary')
         config.add_route('index', '/')
 
-        config.add_view(search_view, route_name='search', request_method='GET', renderer='json')
-        config.add_view(reverse_view, route_name='reverse', request_method='GET', renderer='json')
-        config.add_view(itinerary_view, route_name='itinerary', request_method='GET', renderer='json')
-        config.add_view(index_view, route_name='index', request_method='GET', renderer='index.jinja2')
+        config.add_view(search_view, route_name='search',
+                        request_method='GET', renderer='json')
+        config.add_view(reverse_view, route_name='reverse',
+                        request_method='GET', renderer='json')
+        config.add_view(itinerary_view, route_name='itinerary',
+                        request_method='GET', renderer='json')
 
-        config.add_request_method(lambda request: search_adapter, 'search_adapter', reify=True)
-        config.add_request_method(lambda request: navitia_adapter, 'navitia_adapter', reify=True)
-        config.add_request_method(lambda request: brouter_adapter, 'brouter_adapter', reify=True)
-        config.add_request_method(lambda request: ssm_config, 'ssm_config', reify=True)
+        config.add_request_method(
+            lambda request: search_adapter, 'search_adapter', reify=True)
+        config.add_request_method(
+            lambda request: navitia_adapter, 'navitia_adapter', reify=True)
+        config.add_request_method(
+            lambda request: brouter_adapter, 'brouter_adapter', reify=True)
+        config.add_request_method(
+            lambda request: ssm_config, 'ssm_config', reify=True)
         app = config.make_wsgi_app()
     return app
--- simplestreetmap/config.py
+++ simplestreetmap/config.py
@@ -1,28 +1,8 @@
 def get_config():
     return {
-        'general': {
-            'map_style_url': 'https://static.ppsfleet.navy/osm-data/styles/generic-latest.json',
-            'websocket_url': 'ws://127.0.0.1:8765',
-        },
-        'overlays': [
-            {
-                'tiles_url': 'https://tiles.maps.ppsfleet.navy',
-                'source_layer': 'public.cameras',
-                'name': 'cameras'
-            }
-        ],
         'search': {
             'addok': {
                 'url': 'https://search.maps.ppsfleet.navy'
             }
         },
-        'maplibre': {
-            'js': {
-                'url': 'https://static.ppsfleet.navy/js/maplibre-5.5.0.js',
-                'url_pmtiles': 'https://static.ppsfleet.navy/js/pmtiles-4.3.0.js'
-            },
-            'css': {
-                'url': 'https://static.ppsfleet.navy/js/maplibre-5.5.0.css'
-            }
-        }
     }
