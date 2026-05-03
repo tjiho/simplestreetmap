0424878411ef1db61d5e65f95d0feace5b39cdff
tjiho <tjiho@ppsfleet.navy>
2026-05-03 22:16:38 +0200

    wip

---
+++ src/singletons/elevation.js
@@ -0,0 +1,74 @@
+import map from './map.js'
+
+class Elevation {
+    constructor() {
+        map.on('load', () => {
+            // map.addSource("lidar", {
+            //     type: 'raster-dem',
+            //     url: 'pmtiles://https://static.ppsfleet.navy/osm-data/aubin.pmtiles',
+            //     encoding: 'terrarium',
+            //     tileSize: 512
+            // })
+
+            // map.addLayer({
+            //     "id": "lidar",
+            //     type: 'hillshade',
+            //     source: 'lidar',
+            //     paint: {
+            //         'hillshade-method': 'multidirectional',
+            //         'hillshade-highlight-color': ['#FF4000', '#FFFF00', '#40FF00', '#00FF80'],
+            //         'hillshade-shadow-color': ['#00BFFF', '#0000FF', '#BF00FF', '#FF0080'],
+            //         'hillshade-illumination-direction': [270, 315, 0, 45],
+            //         'hillshade-illumination-altitude': [30, 30, 30, 30],
+            //     }
+            // })
+
+            // map.addLayer({
+            //     id: 'hillshade',
+            //     type: 'hillshade',
+            //     source: 'lidar',
+            //     paint: { 'hillshade-exaggeration': 0.5 }
+            // })
+
+            //map.setTerrain({ source: 'lidar', exaggeration: 1 });
+
+            // map.addControl(new maplibregl.TerrainControl({
+            //     source: 'lidar',
+            //     exaggeration: 1
+            // }));
+
+            map.addSource("bdTopo", {
+                type: "vector",
+                tiles: [
+                    "https://data.geopf.fr/tms/1.0.0/BDTOPO/{z}/{x}/{y}.pbf"
+                ]
+            })
+
+            // map.addLayer({
+            //     id: "batiments_ign",
+            //     type: "fill",
+            //     source: "bdTopo",
+            //     'source-layer': "batiment",
+            //     paint: {
+            //         "fill-color": "rgb(255, 0, 0)",
+            //     }
+            // })
+
+            map.addLayer({
+                id: "batiments_ign_hauteur",
+                type: "fill-extrusion",
+                source: "bdTopo",
+                'source-layer': "batiment",
+                paint: {
+                    'fill-extrusion-height': ['get', 'hauteur'],
+                    'fill-extrusion-opacity': 1,
+                    'fill-extrusion-color': "#F1E8D1"
+                }
+            })
+
+        })
+    }
+}
+
+const elevation = new Elevation()
+export default elevation
