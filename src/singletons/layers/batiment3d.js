2ca91a92756148e82ead07422a541a7dc92329db
Tom Darboux <tjiho@ppsfleet.navy>
2026-05-08 23:52:29 +0200

    Show 3d batiment before labels on map

--- src/singletons/layers/batiment3d.js
+++ src/singletons/layers/batiment3d.js
@@ -8,20 +8,23 @@ class Batiment3d extends AbstractLayer {
       tiles: ["https://data.geopf.fr/tms/1.0.0/BDTOPO/{z}/{x}/{y}.pbf"],
     });
 
-    this.addLayer({
-      id: "batiments_ign_hauteur",
-      type: "fill-extrusion",
-      source: "bdTopo",
-      "source-layer": "batiment",
-      paint: {
-        "fill-extrusion-height": ["get", "hauteur"],
-        "fill-extrusion-opacity": 1,
-        "fill-extrusion-color": "#F1E8D1",
+    this.addLayer(
+      {
+        id: "batiments_ign_hauteur",
+        type: "fill-extrusion",
+        source: "bdTopo",
+        "source-layer": "batiment",
+        paint: {
+          "fill-extrusion-height": ["get", "hauteur"],
+          "fill-extrusion-opacity": 1,
+          "fill-extrusion-color": "#F1E8D1",
+        },
+        layout: {
+          visibility: "none",
+        },
       },
-      layout: {
-        visibility: "none",
-      },
-    });
+      "park-label",
+    );
 
     // map.addSource("lidar", {
     //     type: 'raster-dem',
