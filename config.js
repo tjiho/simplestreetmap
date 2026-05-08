94865c2c2d3e56cb7af9a537a39f4f5e8e7e06bf
Tom Darboux <tjiho@ppsfleet.navy>
2026-05-08 02:06:37 +0200

    Update screenshot

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-const BASE_MAP_URL = 'https://static.ppsfleet.navy/osm-data/beta.json'
+const BASE_MAP_URL = "https://static.ppsfleet.navy/osm-data/beta.json";
 
 const BASE_SEARCH_URL = (query, lat, lng) =>
-  `https://search.maps.ppsfleet.navy/search/?q=${query}&lat=${lat}&lon=${lng}`
+  `https://search.maps.ppsfleet.navy/search/?q=${query}&lat=${lat}&lon=${lng}`;
 
 const BASE_REVERSE_URL = (lat, lng) =>
-  `https://search.maps.ppsfleet.navy/reverse?lat=${lat}&lon=${lng}`
-
-const BASE_OVERPASS_URL = 'https://overpass-api.de/api/interpreter'
+  `https://search.maps.ppsfleet.navy/reverse?lat=${lat}&lon=${lng}`;
